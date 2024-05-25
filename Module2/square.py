f2cfd8b0154b1f6dcaddaaaf303df54f3bdc4457
kjfreidhof <kjfreidhof@home.home>
2024-05-25 17:48:17 +0000

    Added square.py a new python program to module 2

---
+++ Module2/square.py
@@ -0,0 +1,18 @@
+# Author: Kyle Freidhof
+# Created: May, 25 2024
+# Credits: https://www.programiz.com/python-programming/examples/square-root
+# About: A Simple program to solve the square root of a number 
+
+# Define the function named it main
+def main():
+    # Ask for user input and store it in the varriable
+    n = eval(input("Enter a number: "))
+    # Then solve the square root of the number 
+    sqrt = n ** 0.5 
+    # Then print out the answer
+    print("The square root is", sqrt)
+
+# Execute the function
+main()
+
+
