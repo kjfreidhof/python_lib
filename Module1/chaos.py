2cf55f2da4e1007957c49acce75b98b331443526
kjfreidhof <kjfreidhof@home.home>
2024-05-25 08:24:13 +0000

    Made Modifications

--- Module1/chaos.py
+++ Module1/chaos.py
@@ -1,4 +1,8 @@
-# Here is a basic program that does chaotic things 
+# Author: Kyle Freidhof
+# Created: May, 25 2024
+# License: MIT
+# Credits: Python Programing An Introduction to computer science John Zelle Third Edition 
+# About Here is a basic program that does chaotic things 
 
 def main():
     print("A Program that does chaotic things")
